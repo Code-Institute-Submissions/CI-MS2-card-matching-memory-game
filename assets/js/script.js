09d7999bc31160424861305ba9b0f27a066a47b7
anteCedens <jaksa.juric@yahoo.com.hr>
2020-07-17 18:14:20 +0000

    Setting the isBoardCleared function (used in flipTile function)

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,6 @@ let firstTile;
 let secondTile;
 let numberOfTiles = document.getElementsByClassName("tile").length;
 
-
 function flipTile() {
   if (lockBoard) return;
   if (this === firstTile) return;
@@ -29,13 +28,12 @@ function flipTile() {
   isBoardCleared();
 }
 
-
 function checkForMatch() {
-  let isMatch = firstTile.dataset.pairingnumber === secondTile.dataset.pairingnumber;
+  let isMatch =
+    firstTile.dataset.pairingnumber === secondTile.dataset.pairingnumber;
 
   isMatch ? disableTiles() : flipTilesBack();
 }
-
 
 function disableTiles() {
   firstTile.removeEventListener("click", flipTile);
@@ -43,7 +41,6 @@ function disableTiles() {
 
   resetBoard();
 }
-
 
 function flipTilesBack() {
   lockBoard = true;
@@ -56,8 +53,27 @@ function flipTilesBack() {
   }, 1500);
 }
 
-
 function resetBoard() {
   [isOneTileFlipped, lockBoard] = [false, false];
   [firstTile, secondTile] = [null, null];
+}
+
+function isBoardCleared() {
+  if (numberOfTiles === document.getElementsByClassName("flipped").length) {
+    setTimeout(() => {
+      alert("Congratulations!! ... The board will be reshuffled for you!");
+    }, 500);
+
+    setTimeout(() => {
+      tiles.forEach((tile) => {
+        let randomPositioning = Math.floor(Math.random() * numberOfTiles);
+        tile.style.order = randomPositioning;
+      });
+      tiles.forEach((tile) => tile.classList.remove("flipped"));
+      resetBoard();
+      tiles.forEach((tile) => tile.addEventListener("click", flipTile));
+    }, 1500);
+  } else {
+    return;
+  }
 }
