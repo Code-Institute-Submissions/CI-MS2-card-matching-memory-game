2967e767a1e49c450e163926b974635be1c30d5c
anteCedens <jaksa.juric@yahoo.com.hr>
2020-07-17 18:03:41 +0000

    Setting the flipTile function in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,26 @@ let isOneTileFlipped = false;
 let firstTile;
 let secondTile;
 let numberOfTiles = document.getElementsByClassName("tile").length;
+
+
+function flipTile() {
+  if (lockBoard) return;
+  if (this === firstTile) return;
+
+  this.classList.add("flipped");
+
+  if (!isOneTileFlipped) {
+    // first click
+    isOneTileFlipped = true;
+    firstTile = this;
+
+    return;
+  }
+
+  // second click
+  secondTile = this;
+
+  checkForMatch();
+
+  isBoardCleared();
+}
